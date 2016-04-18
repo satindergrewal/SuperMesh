52df247ebd710949ae518f474bc4498994e2794c
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-04-19 02:01:57 +1200

    test

--- public/js/network.js
+++ public/js/network.js
@@ -327,8 +327,27 @@ function UpdateNetSettings(event) {
             dataType: 'JSON',
             success: function(data) {
                 console.log('Data: ' + data);
-            }
-        })
+            },
+            error: function (jqXHR, exception) {
+                var msg = '';
+                if (jqXHR.status === 0) {
+                    msg = 'Not connect.\n Verify Network.';
+                } else if (jqXHR.status == 404) {
+                    msg = 'Requested page not found. [404]';
+                } else if (jqXHR.status == 500) {
+                    msg = 'Internal Server Error [500].';
+                } else if (exception === 'parsererror') {
+                    msg = 'Requested JSON parse failed.';
+                } else if (exception === 'timeout') {
+                    msg = 'Time out error.';
+                } else if (exception === 'abort') {
+                    msg = 'Ajax request aborted.';
+                } else {
+                    msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                }
+                    console.log(msg);
+                    console.log(jqXHR);
+        }
         .done(function( response ) {
             console.log(response);
 
