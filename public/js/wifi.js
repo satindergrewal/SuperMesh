3dd55cdef1da1ef063461b1875c3a2694c91bd39
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-04-23 10:57:45 +1200

    test

--- public/js/wifi.js
+++ public/js/wifi.js
@@ -63,7 +63,7 @@ function populateTable() {
                 tableContent += '<td class="hidden-xs">' + this.frequency + '</td>';
                 tableContent += '<td class="hidden-xs hidden-sm" style="color: ' + wifi_sec_color + ';">' + wifi_security + ' ' + this.security + '</td>';
                 tableContent += '<td class="text-center">';
-                tableContent += '<button class="btn btn-sm btn-minw btn-rounded btn-default" type="button">Connect</button>';
+                tableContent += '<button class="btn btn-xs btn-minw btn-rounded btn-primary" type="button">Connect</button>';
                 tableContent += '</td>';
             tableContent += '</tr>';
 
