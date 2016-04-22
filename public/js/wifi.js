595a68d12d01d39e9bdc5cda7b584590dd823788
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-04-22 20:52:13 +1200

    test

--- public/js/wifi.js
+++ public/js/wifi.js
@@ -33,11 +33,26 @@ function populateTable() {
         $('#userList table tbody').html(tableContent);*/
 
         $.each(data, function(){
-            console.log('--------------------');
+            /*console.log('--------------------');
             console.log(this.signal);
             console.log(this.ssid);
             console.log(this.frequency);
-            console.log(this.security);
+            console.log(this.security);*/
+
+            tableContent += '<tr>';
+                tableContent += '<td class="text-center">' + this.signal + '</i></td>';
+                tableContent += '<td class="font-w600">' + this.ssid + '</td>';
+                tableContent += '<td class="hidden-xs">' + this.frequency + '</td>';
+                tableContent += '<td class="hidden-xs hidden-sm">' + this.security + '</td>';
+                tableContent += '<td class="text-center">';
+                tableContent += ;
+                tableContent += '</td>';
+            tableContent += '</tr>';
+
+        // Inject the whole content string into our existing HTML table
+        $('#wifilist table tbody').html(tableContent);
+
+
         });
 
         console.log(data);
