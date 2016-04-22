c40b0cd2064b536828267606038708ec10d3eb78
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-04-22 21:16:35 +1200

    test

--- public/js/wifi.js
+++ public/js/wifi.js
@@ -17,6 +17,9 @@ function populateTable() {
     // Empty content string
     var tableContent = '';
 
+
+
+
     // jQuery AJAX call for JSON
     $.getJSON( 'wifi/iwlist', function( data ) {
 
@@ -52,10 +55,50 @@ function populateTable() {
         // Inject the whole content string into our existing HTML table
         $('#wifilist table tbody').html(tableContent);
 
+        console.log(tableContent);
+
 
         });
 
         console.log(data);
         console.log(data[0]);
+    });
+};
+
+
+
+// Userlist data array for filling in info box
+var userListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+    // Populate the user table on initial page load
+    populateTable();
+
+});
+
+// Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+
+    // Empty content string
+    var tableContent = '';
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/users/userlist', function( data ) {
+
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+            tableContent += '<tr>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+            tableContent += '<td>' + this.email + '</td>';
+            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '</tr>';
+        });
+
+        // Inject the whole content string into our existing HTML table
+        $('#userList table tbody').html(tableContent);
     });
 };
