409e951b28715cdb9d84a4b7445d374b1ea29f22
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-05-23 21:30:49 +1200

    install helper

---
+++ private/js/install_helper.js
@@ -0,0 +1,21 @@
+var SuperMesh = require("./app_functions.js");
+var lshw = require("./lshw.js");
+
+lshw.status(function(err, status) {
+	//console.log(status);
+	for (i = 0; i < status.length; i++) {
+		//console.log(status[i]);
+		if ( status[i].network === 'wlan0' ) {
+			if ( status[i].driver === 'brcmfmac' ) {
+				console.log(status[i].driver);
+				SuperMesh.RunCmd('sudo rm -f /usr/sbin/hostapd; sudo ln -s /usr/sbin/hostapd_original /usr/sbin/hostapd; sudo systemctl restart isc-dhcp-server; su$
+			} else if ( status[i].driver === 'rtl8192cu' ) {
+				console.log(status[i].driver);
+				SuperMesh.RunCmd('sudo rm -f /usr/sbin/hostapd; sudo ln -s /usr/sbin/hostapd_edimax_bgn /usr/sbin/hostapd; sudo systemctl restart isc-dhcp-server; su$
+			} else {
+				console.log(status[i].driver);
+				SuperMesh.RunCmd('sudo rm -f /usr/sbin/hostapd; sudo ln -s /usr/sbin/hostapd_original /usr/sbin/hostapd; sudo systemctl restart isc-dhcp-server; su$
+			}
+		}
+	}
+});
