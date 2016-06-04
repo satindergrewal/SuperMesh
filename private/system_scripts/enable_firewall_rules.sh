9036f3f55b3ab4525c7f34516c195838fe32cf98
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-06-04 22:20:29 +1200

    test

---
+++ private/system_scripts/enable_firewall_rules.sh
@@ -0,0 +1,39 @@
+#Enable IPv4 forwarding
+#Enable Masquerading on eth0 interface, the Internet Interneface
+#Enable Masquerading on eth0 interface, the Internet Interneface
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"; sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE
+
+# Between eth0 (Internet) <-> wlan0 (Local LAN)
+#Forward any packet coming from eth0 (Internet) interface to wlan0 (Access Point) interface
+#Forward any packet coming from wlan0 (Access Point) interface to eth0 (Internet) interface
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT; sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+
+# Between eth0 (Internet) <-> eth1 (Local LAN)
+#Forward any packet coming from eth0 (Internet) interface to eth1 (Access Point) interface
+#Forward any packet coming from eth1 (Access Point) interface to eth0 (Internet) interface
+sudo iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT; sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+
+# Between wlan1 (Internet) <-> wlan0 (Local LAN)
+#Forward any packet coming from wlan1 (Internet) interface to wlan0 (Access Point) interface
+#Forward any packet coming from wlan0 (Access Point) interface to wlan1 (Internet) interface
+sudo iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT; sudo iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
+
+# Between wlan1 (Internet) <-> eth1 (Local LAN)
+#Forward any packet coming from wlan1 (Internet) interface to eth1 (Access Point) interface
+#Forward any packet coming from eth1 (Access Point) interface to wlan1 (Internet) interface
+sudo iptables -A FORWARD -i wlan1 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT; sudo iptables -A FORWARD -i eth1 -o wlan1 -j ACCEPT
+
+#Mre NAT rules
+#Add firewall rule to allow access to SSH through internal network from where the home users connect to. Both LAN (eth1) and wireless (wlan0)
+sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 22 -j REDIRECT --to-ports 22; sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-ports 22
+
+#Add firewall rule to allow internal users to do DNS queries
+sudo iptables -t nat -A PREROUTING -i eth1 -p udp --dport 53 -j REDIRECT --to-ports 53; sudo iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
+
+#Make sure SuperMesh web interface is ALWAYS accessible to internal users
+sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 3000 -j REDIRECT --to-ports 3000; sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 3000 -j REDIRECT --to-ports 3000
+
+sudo iptables -t nat -A PREROUTING -i eth1 -p udp --dport 9050 -j REDIRECT --to-ports 9050; sudo iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 9050 -j REDIRECT --to-ports 9050
+
+#Save updated iptables rules to ipv4 file
+sudo sh -c "iptables-save > /etc/network/iptables.ipv4.nat"
--- routes/firewall.js
+++ routes/firewall.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var fs = require('fs');
 var sys = require('sys');
+var Promise = require('bluebird');
 
 // SuperMesh app functions
 var SuperMesh = require("../private/js/app_functions.js");
@@ -62,90 +63,7 @@ router.post('/update', function(req, res) {
 
 
 	if ( req.body.iptables4_enable_disable === 'true' ) {
-		// Promise returning functions to execute
-		function enableIPForwarding(){
-			//Enable IPv4 forwarding
-			//Enable Masquerading on eth0 interface, the Internet Interneface
-			//Enable Masquerading on eth0 interface, the Internet Interneface
-			SuperMesh.RunCmd('sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"; sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE');
-			return Promise.resolve(1);
-		}  
-
-		function BetweenEth0Wlan0(res){
-			// Between eth0 (Internet) <-> wlan0 (Local LAN)
-			//Forward any packet coming from eth0 (Internet) interface to wlan0 (Access Point) interface
-			//Forward any packet coming from wlan0 (Access Point) interface to eth0 (Internet) interface
-			SuperMesh.RunCmd('sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT; sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT');
-			return Promise.resolve(res + 1);
-		}  
-
-		function BetweenEth0Eth1(res){
-			// Between eth0 (Internet) <-> eth1 (Local LAN)
-			//Forward any packet coming from eth0 (Internet) interface to eth1 (Access Point) interface
-			//Forward any packet coming from eth1 (Access Point) interface to eth0 (Internet) interface
-			SuperMesh.RunCmd('sudo iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT; sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT');
-			return Promise.resolve(res + 2);
-		}
-
-		function BetweenWlan1Wlan0(res){
-			// Between wlan1 (Internet) <-> wlan0 (Local LAN)
-			//Forward any packet coming from wlan1 (Internet) interface to wlan0 (Access Point) interface
-			//Forward any packet coming from wlan0 (Access Point) interface to wlan1 (Internet) interface
-			SuperMesh.RunCmd('sudo iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT; sudo iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT');
-			return Promise.resolve(res + 2);
-		}
-
-		function BetweenWlan1Eth1(res){
-			// Between wlan1 (Internet) <-> eth1 (Local LAN)
-			//Forward any packet coming from wlan1 (Internet) interface to eth1 (Access Point) interface
-			//Forward any packet coming from eth1 (Access Point) interface to wlan1 (Internet) interface
-			SuperMesh.RunCmd('sudo iptables -A FORWARD -i wlan1 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT; sudo iptables -A FORWARD -i eth1 -o wlan1 -j ACCEPT');
-			return Promise.resolve(res + 2);
-		}
-
-		function OpenPort22(res){
-			//Mre NAT rules
-			//Add firewall rule to allow access to SSH through internal network from where the home users connect to. Both LAN (eth1) and wireless (wlan0)
-			SuperMesh.RunCmd('sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 22 -j REDIRECT --to-ports 22; sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-ports 22');
-			return Promise.resolve(res + 2);
-		}
-
-		function OpenPort53(res){
-			//Add firewall rule to allow internal users to do DNS queries
-			SuperMesh.RunCmd('sudo iptables -t nat -A PREROUTING -i eth1 -p udp --dport 53 -j REDIRECT --to-ports 53; sudo iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53');
-			return Promise.resolve(res + 2);
-		}
-
-		function OpenPort3000(res){
-			//Make sure SuperMesh web interface is ALWAYS accessible to internal users
-			SuperMesh.RunCmd('sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 3000 -j REDIRECT --to-ports 3000; sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 3000 -j REDIRECT --to-ports 3000');
-			return Promise.resolve(res + 2);
-		}
-
-		function OpenPort9050(res){
-			SuperMesh.RunCmd('sudo iptables -t nat -A PREROUTING -i eth1 -p udp --dport 9050 -j REDIRECT --to-ports 9050; sudo iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 9050 -j REDIRECT --to-ports 9050');
-			return Promise.resolve(res + 2);
-		}
-
-		function SaveIPTables(res){
-			//Save updated iptables rules to ipv4 file
-			SuperMesh.RunCmd('sudo sh -c "iptables-save > /etc/network/iptables.ipv4.nat"')
-			//console.log("result:", res);
-		}
-
-		var FirwallActionList = [ enableIPForwarding, BetweenEth0Wlan0, BetweenEth0Eth1, BetweenWlan1Wlan0, BetweenWlan1Eth1, OpenPort22, OpenPort53, OpenPort3000, OpenPort9050, SaveIPTables];
-
-		// Execute a list of Promise return functions in series
-		function pseries(ActionList) {  
-		  var p = Promise.resolve();
-		  return ActionList.reduce(function(pacc, fn) {
-		    return pacc = pacc.then(fn);
-		  }, p);
-		}
-
-		pseries(FirwallActionList);  
-		// result: 4
-
+		SuperMesh.RunCmd('sudo private/system_scripts/enable_firewall_rules.sh');
 	} else if ( req.body.iptables4_enable_disable === 'false' ) {
 		//Enable IPv4 forwarding
 		//Enable Masquerading on eth0 interface, the Internet Interneface
