bc82fce70291aa986fea1a9508fa6f5e7d9a6535
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-06-11 20:35:43 +1200

    Namecoin Service

---
+++ private/3rdparty/NamecoinCoreARM/README.md
@@ -0,0 +1,17 @@
+# Namecoin-Core-ARM
+
+These Namecoin binaries are compiled from Namecoin-Core 0.12 branch:
+https://github.com/namecoin/namecoin-core/blob/0.12
+
+If you want to compile these binaries yourself please follow instructions from here:
+https://github.com/namecoin/namecoin-core/blob/0.12/doc/build-unix.md#arm-cross-compilation
+
+
+## How to install these Namecoin-Core ARM binaries
+
+```shell
+git clone https://github.com/satindergrewal/Namecoin-Core-ARM
+cd Namecoin-Core-ARM
+chmod +x install-nmc.sh
+./install-nmc.sh
+```
---
+++ private/3rdparty/NamecoinCoreARM/install-nmc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+USER="$(who am i | awk '{print $1}')"
+
+sudo cp -av bench_namecoin namecoin-cli namecoind namecoin-tx test_namecoin /usr/local/bin/
+sudo cp -av namecoin.service /etc/systemd/system/
+sudo mkdir -p /media/usb/namecoin
+sudo chown -R "${USER}":"${USER}" /media/usb/namecoin
+echo "rpcuser=pi"  > /media/usb/namecoin/namecoin.conf
+echo "rpcpassword=`openssl rand -hex 20`"  >> /media/usb/namecoin/namecoin.conf
+echo "rpcport=8336" >> /media/usb/namecoin/namecoin.conf
+echo "server=1" >> /media/usb/namecoin/namecoin.conf
+ln -s /media/usb/namecoin $HOME/.namecoin
+sudo sed -i "s/root/${USER}/" /etc/systemd/system/namecoin.service
+sudo systemctl enable namecoin
+sudo systemctl start namecoin
