902900bfde4f77a0215661a405381757d2593d6b
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-06-23 20:35:18 +1200

    test

--- routes/ncdns.js
+++ routes/ncdns.js
@@ -59,7 +59,7 @@ router.post('/update', function(req, res) {
 		//console.log('>>>>>> Runing 3rd')
 		//console.log("result:", res);
 		//Execute promissed spanw child process
-		SuperMesh.RunCmd('sudo cf-agent -K /opt/SuperMesh/private/system_scripts/ncdns_conf.cf; sudo systemctl restart ncdns; sudo rm /etc/ncdns/ncdns.conf.cf-before-edit;');
+		SuperMesh.RunCmd('sudo cf-agent -K /opt/SuperMesh/private/system_scripts/ncdns_conf.cf; sudo systemctl restart ncdns; sudo systemctl restart pdns-recursor; sudo rm /etc/ncdns/ncdns.conf.cf-before-edit;');
 	}
 
 	var UpdateSteps = [ UpdateNCDNSSettings, SetupNCDNSService ];
@@ -74,20 +74,6 @@ router.post('/update', function(req, res) {
 
 	UpdateProcess(UpdateSteps);
 
-	if ( req.body.enable_ncdns_service === 'true' ) {
-		//console.log('-- enable disable ncdns service is true --');
-		//console.log(req.body.enable_ncdns_service);
-		// Enable NCDNS Service
-			console.log('==> Enabling NCDNS to start at system boot, and starting NCDNS Service...')
-			SuperMesh.RunCmd('sudo systemctl daemon-reload; sudo systemctl enable ncdns; sudo systemctl start ncdns');
-	} else if ( req.body.enable_ncdns_service === 'false' ) {
-		//console.log('-- enable disable ncdns service is false --');
-		//console.log(req.body.enable_ncdns_service);
-		// Disable NCDNS Service
-			console.log('==> Disabling NCDNS to start at system boot, and stopting NCDNS Service...')
-			SuperMesh.RunCmd('sudo systemctl daemon-reload; sudo systemctl disable ncdns; sudo systemctl stop ncdns');
-	}
-
 	res.end('{"msg": "success","result": "result"}');
 	
 });
@@ -96,7 +82,7 @@ router.post('/update', function(req, res) {
 // Enable NCDNS Service
 router.get('/enablencdns', function(req, res) {
 	// Enable NCDNS service to start at boot time, then start NCDNS service
-	SuperMesh.RunCmd('sudo systemctl daemon-reload; sudo systemctl enable ncdns; sudo systemctl start ncdns');
+	SuperMesh.RunCmd('sudo systemctl daemon-reload; sudo systemctl enable ncdns; sudo systemctl start ncdns; sudo systemctl restart pdns-recursor');
 
 	res.end('{"msg": "success","result": "result"}');
 });
@@ -104,7 +90,7 @@ router.get('/enablencdns', function(req, res) {
 // Disable NCDNS Service
 router.get('/disablencdns', function(req, res) {
 	// Disable NCDNS service to start at boot time, then stop NCDNS service
-	SuperMesh.RunCmd('sudo systemctl daemon-reload; sudo systemctl disable ncdns; sudo systemctl stop ncdns');
+	SuperMesh.RunCmd('sudo systemctl daemon-reload; sudo systemctl disable ncdns; sudo systemctl stop ncdns; sudo systemctl restart pdns-recursor');
 
 	res.end('{"msg": "success","result": "result"}');
 });
@@ -112,7 +98,7 @@ router.get('/disablencdns', function(req, res) {
 // Restart NCDNS Service
 router.get('/restartncdns', function(req, res) {
 	// Restart NCDNS service
-	SuperMesh.RunCmd('sudo systemctl daemon-reload; sudo systemctl restart ncdns');
+	SuperMesh.RunCmd('sudo systemctl daemon-reload; sudo systemctl restart ncdns; sudo systemctl restart pdns-recursor');
 
 	res.end('{"msg": "success","result": "result"}');
 });
